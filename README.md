d1210cbc343fa625895d49df4ca00b402c4e8d3c
MartinReinhart <reinhart19_91@hotmail.com>
2022-12-03 19:19:25 +0100

    completed day 3

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Solutions for [Advent of Code 2022][aoc]. This project uses Java 17.
 
 ## Progress
 
-| Day                                                                                                                | Part One | Part Two |
-|--------------------------------------------------------------------------------------------------------------------|:--------:|:--------:|
-| ✔ [Day 1: Calorie Counting](https://github.com/Mom0aut/AoC2022/blob/master/src/main/java/at/aoc/day1/Day1.java)    |    🌟    |    🌟    |
-| ✔ [Day 2: Rock Paper Scissors](https://github.com/Mom0aut/AoC2022/blob/master/src/main/java/at/aoc/day2/Day2.java) |    🌟    |    🌟    |
-
+| Day                                                                                                                    | Part One | Part Two |
+|------------------------------------------------------------------------------------------------------------------------|:--------:|:--------:|
+| ✔ [Day 1: Calorie Counting](https://github.com/Mom0aut/AoC2022/blob/master/src/main/java/at/aoc/day1/Day1.java)        |    🌟    |    🌟    |
+| ✔ [Day 2: Rock Paper Scissors](https://github.com/Mom0aut/AoC2022/blob/master/src/main/java/at/aoc/day2/Day2.java)     |    🌟    |    🌟    |
+| ✔ [Day 3: Rucksack Reorganization](https://github.com/Mom0aut/AoC2022/blob/master/src/main/java/at/aoc/day3/Day3.java) |    🌟    |    🌟    |
---
+++ src/main/java/at/aoc/day3/Day3.java
@@ -0,0 +1,82 @@
+package at.aoc.day3;
+
+import at.aoc.util.BufferReaderUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Day3 {
+
+    String points = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static void main(String[] args) throws IOException {
+        Day3 day3 = new Day3();
+        //        day3.partOne("day3/bigData.txt");
+        day3.partTwo("day3/bigData.txt");
+    }
+
+    public void partOne(String filename) throws IOException {
+        List<Character> commonItems = new ArrayList<>();
+        AtomicInteger priorities = new AtomicInteger();
+        BufferedReader reader = BufferReaderUtil.readFile(filename);
+        String line = reader.readLine();
+        while (line != null) {
+            String firstCompartment = line.substring(0, line.length() / 2);
+            String secondCompartment = line.substring(line.length() / 2);
+            char[] firstChars = firstCompartment.toCharArray();
+            for (char firstChar : firstChars) {
+                int i = secondCompartment.indexOf(firstChar);
+                if (i >= 0) {
+                    System.out.println("Found Match: " + secondCompartment.charAt(
+                            i) + " " + firstCompartment + ":" + secondCompartment + "\n");
+                    commonItems.add(secondCompartment.charAt(i));
+                    break;
+                }
+            }
+            line = reader.readLine();
+        }
+        reader.close();
+        calculatePriorities(commonItems, priorities);
+        System.out.println("Priorities: " + priorities + "\n");
+    }
+
+    private void calculatePriorities(List<Character> commonItems, AtomicInteger priorities) {
+        commonItems.forEach(item -> {
+            int delta = points.indexOf(item) + 1;
+            priorities.addAndGet(delta);
+        });
+    }
+
+
+    public void partTwo(String filename) throws IOException {
+        List<Character> commonItems = new ArrayList<>();
+        AtomicInteger priorities = new AtomicInteger();
+        BufferedReader reader = BufferReaderUtil.readFile(filename);
+        String firstLine = reader.readLine();
+        String secondLine = reader.readLine();
+        String thirdLine = reader.readLine();
+        while (thirdLine != null) {
+            char[] firstChars = firstLine.toCharArray();
+            for (char firstChar : firstChars) {
+                int i = secondLine.indexOf(firstChar);
+                int j = thirdLine.indexOf(firstChar);
+                if (i >= 0 && j >= 0) {
+                    System.out.println("Found Char in 3 Lines: " + firstChar + "\n");
+                    commonItems.add(firstChar);
+                    break;
+                }
+            }
+            firstLine = reader.readLine();
+            secondLine = reader.readLine();
+            thirdLine = reader.readLine();
+        }
+        reader.close();
+        calculatePriorities(commonItems, priorities);
+        System.out.println("Priorities: " + priorities + "\n");
+    }
+
+
+}
